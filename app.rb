2e65230ec543bc31294cce30ae7abb4bd14df155
jefk <kjeffk@gmail.com>
2012-03-22 09:18:11 -0700

    stub endpoints

--- app.rb
+++ app.rb
@@ -5,3 +5,16 @@ require 'sinatra/reloader' if development?
 get '/' do
   "foodpr0n"
 end
+
+post '/pron' do
+  # rotate the current image
+  "nyi"
+end
+
+put '/img-src' do
+  "nyi"
+end
+
+post '/img-src/decay' do
+  "nyi"
+end
