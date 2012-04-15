92de7e202c1d30052878166c2dea560fd3d13f99
jeff kahn <kjeffk@gmail.com>
2012-04-15 14:54:52 -0700

    started twitter search class

--- Gemfile
+++ Gemfile
@@ -6,10 +6,8 @@ gem 'sequel', '~> 3.33.0'
 gem 'sqlite3', '~> 1.3.5'
 gem 'yajl-ruby', '~> 1.1.0'
 
-gem 'rake', '~> 0.9.2.2'
-
 gem 'twitter', '~> 2.2.0'
 
 # dev
-gem 'rspec', '~> 2.9.0'
+gem 'rspec'
 gem 'sinatra-contrib', '~> 1.3.1'
---
+++ lib/twitter_robot.rb
@@ -0,0 +1,61 @@
+
+require 'twitter'
+
+class TwitterRobot
+
+  def initialize(foodpron_base)
+    @foodpron_base = foodpron_base
+    @max_id = 0
+    @sleep_time = 10
+  end
+
+  def go(q)
+    puts "starting up the twitter robot"
+    while true
+      puts "waiting for #{@sleep_time} seconds"
+      sleep @sleep_time + rand
+      tweets = tweets_since(q, @max_id)
+      tweets.sort_by! {|t| t.created_at}
+      @max_id = tweets.last.id
+      foodpron(tweets)
+      @sleep_time = auto_scale(tweets.map {|t| t.created_at})
+    end
+  end
+
+  private
+
+  def tweets_since(q, max_id)
+    tweets = Twitter.search(q, :result_type => 'recent', :since_id => max_id)
+    puts "query '#{q}': recieved #{tweets.lenght} tweets"
+    tweets
+  end
+
+  def foodpron(tweets)
+    tweets.each do |tweet|
+      urls = extract_urls(tweet.text)
+    end
+#    tweet.from_user
+#    tweet.id
+#    tweet.text
+#    tweet.created_at
+#    puts "#{status.from_user}\t#{status.text}"
+  end
+
+  def extract_urls(s)
+    puts s
+    puts s.scan(%r|http://.*[\w$]|)
+  end
+
+  def auto_scale(times)
+    # will check again in half time time it took for these tweets to come in
+    total_time = Time.now - times.first.created_at
+    p total_time
+    wait_time = total_time / 2
+    bound(wait_time, 10, 100)
+  end
+
+  def bound(x, min, max)
+    [x, min, max].sort[1]
+  end
+
+end
