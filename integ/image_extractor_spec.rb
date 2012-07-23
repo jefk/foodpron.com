b5eac734bc0c97d7bca0c4b55498937deebd2655
jefk <kjeffk@gmail.com>
2012-07-22 22:03:23 -0700

    largest image extracted

--- integ/image_extractor_spec.rb
+++ integ/image_extractor_spec.rb
@@ -5,7 +5,7 @@ describe ImageExtractor do
 
   it "largest_image" do
     url = 'http://oishidiah.tumblr.com/post/27021433563/post-work-out-meal-hawaiian-bbq-chicken'
-    p ImageExtractor.largest_image(url)
+    ImageExtractor.largest_image(url).should == 'http://25.media.tumblr.com/tumblr_m70zu1I8Gl1qe93ffo1_500.jpg'
   end
 
 end
--- lib/image_extractor.rb
+++ lib/image_extractor.rb
@@ -9,7 +9,27 @@ class ImageExtractor
     html_src = Net::HTTP.get URI.parse(url)
     doc = Nokogiri::HTML html_src
     imgs = doc.css('img').map{ |node| node.attribute('src').to_s }
-    
+    imgs.sort_by{ |url| content_length(url) }.last
+  end
+
+  def self.content_length(url)
+    fetch(url).content_length
+  end
+
+  def self.fetch(uri_str, limit = 10)
+    raise "HTTP redirect too deep" if limit == 0
+
+    url = URI.parse(uri_str)
+    req = Net::HTTP::Head.new(url.path)
+    response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+    case response
+    when Net::HTTPSuccess
+      response
+    when Net::HTTPRedirection
+      fetch(response['location'], limit - 1)
+    else
+      response.error!
+    end
   end
 
 end
