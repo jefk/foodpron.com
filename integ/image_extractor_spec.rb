41bf30e06303eccb564359442008275ff71f7eaa
jefk <speedracer111@gmail.com>
2012-11-20 19:42:28 -0800

    update test case

--- integ/image_extractor_spec.rb
+++ integ/image_extractor_spec.rb
@@ -4,8 +4,9 @@ require 'spec_helper'
 describe ImageExtractor do
 
   it "largest_image" do
-    url = 'http://oishidiah.tumblr.com/post/27021433563/post-work-out-meal-hawaiian-bbq-chicken'
-    ImageExtractor.largest_image(url).should == 'http://25.media.tumblr.com/tumblr_m70zu1I8Gl1qe93ffo1_500.jpg'
+    url = 'http://instagram.com/p/SRrbctzaov/'
+    ImageExtractor.largest_image(url).should ==
+      'http://distilleryimage3.instagram.com/268b0124338a11e29df022000a1fb07c_7.jpg'
   end
 
 end
